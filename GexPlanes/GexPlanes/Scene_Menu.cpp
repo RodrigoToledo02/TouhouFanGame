6f5a97fb3d149c9fb95438204514d2ae2f034cec
Rodrigo Toledo Castillo <RTOLEDOCASTILLO01@mynbcc.ca>
2025-01-31 10:34:43 -0400

    Basic updates to menu

--- GexPlanes/GexPlanes/Scene_Menu.cpp
+++ GexPlanes/GexPlanes/Scene_Menu.cpp
@@ -14,34 +14,32 @@ Scene_Menu::Scene_Menu(GameEngine* gameEngine)
 	init();
 }
 
-
-
-void Scene_Menu:: init()
+void Scene_Menu::init()
 {
 	MusicPlayer::getInstance().play("menuTheme");
 	MusicPlayer::getInstance().setVolume(5);
 
-    registerAction(sf::Keyboard::W,			"UP");
-    registerAction(sf::Keyboard::Up,		"UP");
-    registerAction(sf::Keyboard::S,			"DOWN");
-    registerAction(sf::Keyboard::Down,	 	"DOWN");
-	registerAction(sf::Keyboard::D,			"PLAY");
-	registerAction(sf::Keyboard::Escape,	"QUIT");
+	registerAction(sf::Keyboard::W, "UP");
+	registerAction(sf::Keyboard::Up, "UP");
+	registerAction(sf::Keyboard::S, "DOWN");
+	registerAction(sf::Keyboard::Down, "DOWN");
+	registerAction(sf::Keyboard::D, "PLAY");
+	registerAction(sf::Keyboard::Right, "PLAY");
+	registerAction(sf::Keyboard::Escape, "QUIT");
 
-	m_title = "Gex Planes";
-	m_menuStrings.push_back("Level 1");
-	m_menuStrings.push_back("Level 2");
-	m_menuStrings.push_back("Level 3");
+	m_title = "TOUHOU: Darkness of the Void";
+	m_menuStrings.emplace_back("Start");
+	m_menuStrings.emplace_back("Options");
+	m_menuStrings.emplace_back("Quit");
 
-	m_levelPaths.push_back("../level1.txt");
-	m_levelPaths.push_back("../level1.txt");
-	m_levelPaths.push_back("../level1.txt");
+	m_levelPaths.emplace_back("../level1.txt");
+	m_levelPaths.emplace_back("../level1.txt");
+	m_levelPaths.emplace_back("../level1.txt");
 
 	m_menuText.setFont(Assets::getInstance().getFont("main"));
 
 	const size_t CHAR_SIZE{ 64 };
 	m_menuText.setCharacterSize(CHAR_SIZE);
-
 }
 
 void Scene_Menu::update(sf::Time dt)
@@ -49,41 +47,67 @@ void Scene_Menu::update(sf::Time dt)
 	_entityManager.update();
 }
 
-
 void Scene_Menu::sRender()
 {
-	 
 	sf::View view = _game->window().getView();
-	view.setCenter(_game->window().getSize().x / 2.f, _game->window().getSize().y / 2.f);
+	sf::Vector2u windowSize = _game->window().getSize();
+	view.setCenter(windowSize.x / 2.f, windowSize.y / 2.f);
 	_game->window().setView(view);
 
 	static const sf::Color selectedColor(255, 255, 255);
 	static const sf::Color normalColor(0, 0, 0);
 
+	const unsigned int minFontSize = 32;
+	const unsigned int maxFontSize = 64;
+
+	unsigned int fontSize = std::max(minFontSize, std::min(maxFontSize, windowSize.y / 15));
+	m_menuText.setCharacterSize(fontSize);
+
 	static const sf::Color backgroundColor(100, 100, 255);
-
-	sf::Text footer("UP: W    DOWN: S   PLAY:D    QUIT: ESC", 
-		Assets::getInstance().getFont("main"), 20);
-	footer.setFillColor(normalColor);
-	footer.setPosition(32, 700);
-
 
 	m_menuText.setFillColor(normalColor);
 	m_menuText.setString(m_title);
-	m_menuText.setPosition(10, 10);
+	sf::FloatRect titleBounds = m_menuText.getLocalBounds();
+	m_menuText.setOrigin(titleBounds.width / 2, titleBounds.height / 2);
+	m_menuText.setPosition(windowSize.x / 2.f, windowSize.y * 0.15f);
+
+	float maxTitleWidth = windowSize.x * 0.8f;
+	if (titleBounds.width > maxTitleWidth)
+	{
+		float scale = maxTitleWidth / titleBounds.width;
+		m_menuText.setScale(scale, scale);
+	}
+	else
+	{
+		m_menuText.setScale(1.f, 1.f);
+	}
 	_game->window().draw(m_menuText);
+
+	const float menuStartY = windowSize.y * 0.3f;
+	const float menuSpacing = windowSize.y * 0.1f;
 
 	for (size_t i{ 0 }; i < m_menuStrings.size(); ++i)
 	{
 		m_menuText.setFillColor((i == m_menuIndex ? selectedColor : normalColor));
-		m_menuText.setPosition(32, 32 + (i+1) * 96);
 		m_menuText.setString(m_menuStrings.at(i));
+
+		sf::FloatRect textBounds = m_menuText.getLocalBounds();
+		m_menuText.setOrigin(textBounds.width / 2, textBounds.height / 2);
+		m_menuText.setPosition(windowSize.x / 2.f, menuStartY + (i * menuSpacing));
+
+		if (textBounds.width > windowSize.x * 0.8f)
+		{
+			float scale = (windowSize.x * 0.8f) / textBounds.width;
+			m_menuText.setScale(scale, scale);
+		}
+		else
+		{
+			m_menuText.setScale(1.f, 1.f);
+		}
+
 		_game->window().draw(m_menuText);
-	} 
-
-	_game->window().draw(footer);
+	}
 }
-
 
 void Scene_Menu::sDoAction(const Command& action)
 {
@@ -92,7 +116,7 @@ void Scene_Menu::sDoAction(const Command& action)
 		if (action.name() == "UP")
 		{
 			m_menuIndex = (m_menuIndex + m_menuStrings.size() - 1) % m_menuStrings.size();
-		} 
+		}
 		else if (action.name() == "DOWN")
 		{
 			m_menuIndex = (m_menuIndex + 1) % m_menuStrings.size();
@@ -106,5 +130,4 @@ void Scene_Menu::sDoAction(const Command& action)
 			onEnd();
 		}
 	}
-
 }
